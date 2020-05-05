537cecc5d19bd28db7856b152781793855d1dc08
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-05-05 11:38:09 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Balbir Chand");
+$("#policyNumber").text("LM2202130");
+$("#startDate").text("05-May-2020");
+$("#endDate").text("04-May-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB37D3260");
+$("#make").text("HONDA");
+$("#model").text("SHINE");
+$("#vehicalSubType").text("DRUM BRAKE");
+$("#engineNo").text("07717");
+$("#chassis").text("05687");
